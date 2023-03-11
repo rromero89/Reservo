d92385abe9f56c68f8c8106050a5c8374c0ff81d
rromero89 <manuelrosemary@hotmail.com>
2023-03-11 11:08:17 -0400

    Add CeloChain y CeloTestChain a SUPPORTED_NETWORKS
    Add configuración del Idioma es-VE en i18next

--- src/features/wallet/config.ts
+++ src/features/wallet/config.ts
@@ -1,19 +1,23 @@
+/*
 import { AvalancheChain } from './chains/avalanche';
 import { AvalancheTestChain } from './chains/avalancheTest';
 import { BinanceSmartChain } from './chains/bsc';
 import { BSCTestChain } from './chains/bscTest';
+*/
 /* Add Cadena Celo y CeloTest */
 import { CeloChain } from './chains/celo';
 import { CeloTestChain } from './chains/celoTest';
 /*****************************/
+/*
 import { EthereumMainnetChain } from './chains/ethereum';
 import { GanacheChain } from './chains/ganache';
 import { GoerliTestChain } from './chains/goerliTest';
 import { HardhatChain } from './chains/hardhat';
 import { PolygonChain } from './chains/polygon';
 import { PolygonMumbaiChain } from './chains/polygonMumbai';
+*/
 import { Network } from './models/network/types/Network';
-
+/*
 export const SUPPORTED_NETWORKS: Network[] = [
   CeloChain,
   CeloTestChain,
@@ -28,7 +32,9 @@ export const SUPPORTED_NETWORKS: Network[] = [
   GanacheChain,
   HardhatChain,
 ];
+*/
+export const SUPPORTED_NETWORKS: Network[] = [CeloChain, CeloTestChain];
 //export const DEFAULT_NETWORK = AvalancheChain;
 export const DEFAULT_NETWORK = CeloTestChain;
 export const SIGN_TIMEOUT_IN_SEC = 60;
-export const SLOW_DOWN_IN_MS = 900;
+export const SLOW_DOWN_IN_MS = 1000;
